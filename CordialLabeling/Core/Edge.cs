2540fa027609613f3ee24fc139672febc242b0f2
Michał Ratajczak <michalratajczak2105@gmail.com>
2020-06-11 17:41:09 +0200

    Added graph logical representation.

---
+++ CordialLabeling/Core/Edge.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CordialLabeling.Core
+{
+    public class Edge
+    {
+        public int Index { get; private set; }
+        public int Label => Math.Abs(A.Label - B.Label);
+        public Vertex A { get; private set; }
+        public Vertex B { get; private set; }
+
+        public Edge(int index, Vertex a, Vertex b)
+        {
+            Index = index + 1;
+            A = a;
+            B = b;
+        }
+    }
+}
---
+++ CordialLabeling/Core/Graph.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace CordialLabeling.Core
+{
+    public class Graph
+    {
+        public int N { get; private set; }
+        public int K { get; private set; }
+        public List<Vertex> Vertices { get; private set; }
+        public List<Edge> Edges { get; private set; }
+        public int[][] AdjacencyMatrix { get; private set; }
+
+        public Graph(int n, int k, int[][] adjacencyMatrix)
+        {
+            N = n;
+            K = k;
+            Vertices = new List<Vertex>(n);
+            Edges = new List<Edge>(k);
+            AdjacencyMatrix = adjacencyMatrix;
+
+            for (int i = 0; i < n; i++)
+            {
+                Vertices.Add(new Vertex(i));
+            }
+        }
+
+        private Graph(int n)
+        {
+            Vertices = new List<Vertex>(n);
+            Edges = new List<Edge>(n * (n - 1) / 2);
+            AdjacencyMatrix = new int[n][];
+            N = n;
+
+            for (int i = 0; i < n; i++)
+            {
+                AdjacencyMatrix[i] = new int[n];
+                Vertices.Add(new Vertex(i));
+            }
+        }
+
+        private Graph(int n, int k)
+        {
+            Vertices = new List<Vertex>(n);
+            Edges = new List<Edge>(k);
+            AdjacencyMatrix = new int[n][];
+            N = n;
+            K = k;
+
+            for (int i = 0; i < n; i++)
+            {
+                AdjacencyMatrix[i] = new int[n];
+                Vertices.Add(new Vertex(i));
+            }
+        }
+
+        private void CalculateVertices()
+        {
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (AdjacencyMatrix[i][j] == 1)
+                    {
+                        Vertices[i].Neighbors.Add(Vertices[j]);
+                    }
+                }
+            }
+        }
+
+        private void CalculateEdges()
+        {
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = i + 1; j < N; j++)
+                {
+                    if (AdjacencyMatrix[i][j] == 1)
+                    {
+                        var edge = new Edge(Edges.Count, Vertices[i], Vertices[j]);
+                        Edges.Add(edge);
+                        Vertices[i].Edges.Add(edge);
+                        Vertices[j].Edges.Add(edge);
+                    }
+                }
+            }
+        }
+
+        public bool IsGraphConnected()
+        {
+            List<Vertex> visited = new List<Vertex>();
+            CheckNextVertex(Vertices.First(), visited);
+            return visited.Count == Vertices.Count;
+        }
+
+        private void CheckNextVertex(Vertex vertex, List<Vertex> visited)
+        {
+            if (!visited.Contains(vertex))
+            {
+                visited.Add(vertex);
+            }
+            foreach (var v in vertex.Neighbors)
+            {
+                if (!visited.Contains(v))
+                {
+                    CheckNextVertex(v, visited);
+                }
+            }
+        }
+
+        public static Graph Generate(int n, double p, bool isConnected)
+        {
+            Random random = new Random();
+            Graph graph;
+            do
+            {
+                graph = new Graph(n);
+
+                for (int i = 0; i < n; i++)
+                {
+                    graph.AdjacencyMatrix[i][i] = 0;
+
+                    for (int j = i + 1; j < n; j++)
+                    {
+                        if (random.NextDouble() < p)
+                        {
+                            graph.AdjacencyMatrix[i][j] = 1;
+                            graph.AdjacencyMatrix[j][i] = 1;
+                        }
+                        else
+                        {
+                            graph.AdjacencyMatrix[i][j] = 0;
+                            graph.AdjacencyMatrix[j][i] = 0;
+                        }
+                    }
+                }
+
+                graph.CalculateVertices();
+                graph.CalculateEdges();
+                graph.K = graph.Edges.Count;
+            }
+            while (!graph.IsGraphConnected() && isConnected);
+
+            return graph;
+        }
+
+        public static Graph Generate(int n, int k, bool isConnected)
+        {
+            Random random = new Random();
+            Graph graph;
+            do
+            {
+                graph = new Graph(n, k);
+
+                int index = 0;
+
+                while(index < k)
+                {
+                    int i = random.Next(0, n);
+                    int j = random.Next(0, n);
+
+                    if(graph.AdjacencyMatrix[i][j] != 1)
+                    {
+                        graph.AdjacencyMatrix[i][j] = 1;
+                        graph.AdjacencyMatrix[j][i] = 1;
+                        index++;
+                    }
+                }
+
+                graph.CalculateVertices();
+                graph.CalculateEdges();
+            }
+            while (!graph.IsGraphConnected() && isConnected);
+
+            return graph;
+        }
+
+        public void UpdateVerticesLabel(int[] labels)
+        {
+            for(int i = 0; i < labels.Length; i++)
+            {
+                Vertices[i].Label = labels[i];
+            }
+        }
+
+        public void UpdateVerticesLabel()
+        {
+            var data = File.ReadAllText("output.txt");
+            Regex regex = new Regex(@"(\d)");
+            var matches = regex.Matches(data).ToArray();
+
+            for(int i = 0; i < Vertices.Count; i++)
+            {
+                Vertices[i].Label = int.Parse(matches[i].Value);
+                Debug.WriteLine(matches[i].Value);
+            }           
+        }
+
+        public void ExportToMiniZincFile()
+        {
+            StringBuilder builder = new StringBuilder();
+
+            builder.AppendLine($"n = {N};");
+            builder.AppendLine($"k = {K};");
+            builder.Append($"edges = [");
+
+            foreach(var edge in Edges)
+            {
+                builder.Append($"|{edge.A.Index},{edge.B.Index}");
+            }
+
+            builder.Append($"|];");
+
+            File.WriteAllText("data.dzn", builder.ToString());
+        }
+
+        
+    }
+}
