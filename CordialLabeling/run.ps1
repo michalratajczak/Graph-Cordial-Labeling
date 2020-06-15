36b9f53b09c199f94cc2d65138a9030b5c734df1
Michał Ratajczak <37875804+michalratajczak@users.noreply.github.com>
2020-06-15 17:47:11 +0200

    Create run.ps1 script

---
+++ CordialLabeling/run.ps1
@@ -0,0 +1,23 @@
+$ModelCode = "int: n;
+int: k;
+
+set of int: N = 1..n; 
+set of int: K = 1..k;  
+
+set of int: LABEL = 0..1;
+
+array[N] of var LABEL: vertices; 
+array[K,1..2] of N: edges; 
+
+constraint abs(n - sum(i in N)(vertices[i]) * 2) <= 1;
+constraint abs(k - sum(j in K)(abs(vertices[edges[j,1]] - vertices[edges[j,2]])) * 2) <= 1;
+
+solve satisfy;
+
+output [show(vertices)];"
+
+Start-Process -FilePath "dotnet" -ArgumentList "publish -p:PublishSingleFile=true -r win-x64 -c Release -o `"..\App`"" -WindowStyle Hidden -Wait
+
+'Graphviz', 'MiniZinc', 'Model', 'Result', 'Temp' | ForEach {New-Item -ItemType Directory -Path "..\App\$_"}
+
+$ModelCode | Out-File -FilePath "..\App\Model\CordialLabeling.mzn" -Encoding utf8
